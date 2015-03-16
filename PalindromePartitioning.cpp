a579c704cfcb24965742b50663e902956d5942dd
ak638 <wuhuadong1010@gmail.com>
2015-03-16 17:36:57 +0800

    add minCut

--- PalindromePartitioning.cpp
+++ PalindromePartitioning.cpp
@@ -19,6 +19,37 @@ using namespace std;
 
 class Solution {
 	public:
+		int minCut(string s) {
+			/*
+			 * https://leetcode.com/problems/palindrome-partitioning-ii/
+			 * 判断是否回文，两种思路：1.枚举每个字串，再判断字串是否为回文；2.迭代判断，字串a是回文，前后再包装同一个字符，那么新的字串xax也是回文，记录下来后面使用
+			 */
+			int n = (int)s.size();
+			if (n <= 1) return 0;
+
+			bool bPalin[n+1][n+1]; //bPalin[i][j]表示s[i, j-1]为回文
+			memset(bPalin, 0, sizeof(bool) * (n+1) * (n+1));
+
+			int dp[n+1]; //res[i]表示s[0...i-1]子串最小cut
+			for (int i = 0; i <= n; ++i) dp[i] = i - 1; //初始化为每个子串最大可能的cut数
+			//dp[0] = -1! 解决dp[1] = dp[0] + 1的问题
+
+			for (int i = 2; i <= n; ++i)
+			{
+				for (int j = i-1; j >= 0; --j)
+				{
+					//s[j, i]
+					if (s[i-1] == s[j] && (i - j <= 2 || bPalin[j+1][i-1]))
+					{
+						bPalin[j][i] = true;
+						dp[i] = min(dp[i], dp[j]+1); //s[0,j-1]最小割次数+s[j,i-1]回文次数1
+					}
+				}
+			}
+
+			return dp[n];
+		}
+		
 		vector<vector<string> > partition(string s) {
 			vector<vector<string> > res;
 			vector<string> single;
